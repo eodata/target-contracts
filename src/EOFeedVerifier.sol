c0b90ebc9c9b87e56e89235b884d55eda51e736c
Ariel Laub <ariel.laub@gmail.com>
2024-11-06 17:54:13 +0200

    fix: slither issue

--- src/EOFeedVerifier.sol
+++ src/EOFeedVerifier.sol
@@ -126,25 +126,27 @@ contract EOFeedVerifier is IEOFeedVerifier, OwnableUpgradeable {
         uint256 length = newValidatorSet.length;
         if (length < _minNumOfValidators) revert ValidatorSetTooSmall();
         // delete the slots of the old validators
+        // slither-disable-start costly-operations-inside-a-loop
         if (length < _currentValidatorSetLength) {
             for (uint256 i = length; i < _currentValidatorSetLength; i++) {
-                // slither-disable-next-line costly-operations-inside-a-loop
                 delete _currentValidatorSet[i];
             }
         }
+        // slither-disable-end costly-operations-inside-a-loop
         _currentValidatorSetLength = length;
         _currentValidatorSetHash = keccak256(abi.encode(newValidatorSet));
         uint256 totalPower = 0;
         uint256[2] memory apk = [uint256(0), uint256(0)];
+        // slither-disable-start  calls-inside-a-loop
         for (uint256 i = 0; i < length; i++) {
             if (newValidatorSet[i]._address == address(0)) revert InvalidAddress();
             uint256 votingPower = newValidatorSet[i].votingPower;
             if (votingPower == 0) revert VotingPowerIsZero();
             totalPower += votingPower;
             _currentValidatorSet[i] = newValidatorSet[i];
-            // slither-disable-next-line calls-inside-a-loop
             apk = _bls.ecadd(apk, newValidatorSet[i].g1pk);
         }
+        // slither-disable-end  calls-inside-a-loop
 
         _fullApk = apk;
         _totalVotingPower = totalPower;
