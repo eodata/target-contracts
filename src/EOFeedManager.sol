6aa770eda8f0c2ec3d6b8a6ee534d020a26eb2db
Tetiana Afonina <tetiana@eoracle.io>
2025-01-07 17:09:04 +0000

    feat: added audit info

--- src/EOFeedManager.sol
+++ src/EOFeedManager.sol
@@ -108,7 +108,7 @@ contract EOFeedManager is IEOFeedManager, OwnableUpgradeable, PausableUpgradeabl
      */
     function setFeedVerifier(address feedVerifier) external onlyOwner onlyNonZeroAddress(feedVerifier) {
         _feedVerifier = IEOFeedVerifier(feedVerifier);
-        // @audit-info do we want to add emit FeedVerifierSet(feedVerifier);
+        emit FeedVerifierSet(feedVerifier);
     }
 
     /**
