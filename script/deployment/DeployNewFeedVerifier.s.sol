2cc2e51babbb8e29a1b31f59937927b20097d501
Sergei Liakh <sergei@eoracle.io>
2025-01-10 17:31:06 +0000

    feat: scripts for feed manager upgrade

---
+++ script/deployment/DeployNewFeedVerifier.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+import { stdJson } from "forge-std/Script.sol";
+import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import { FeedVerifierDeployer } from "./base/DeployFeedVerifier.s.sol";
+import { IBLS } from "src/interfaces/IBLS.sol";
+import { EOJsonUtils } from "script/utils/EOJsonUtils.sol";
+
+// Deployment command: FOUNDRY_PROFILE="deployment" forge script script/deployment/DeployNewFeedVerifier.s.sol
+// --rpc-url $RPC_URL --private-key $DEPLOYER_PRIVATE_KEY -vvv --slow --verify --broadcast
+contract DeployNewFeedVerifier is FeedVerifierDeployer {
+    using stdJson for string;
+
+    function run() external {
+        vm.startBroadcast();
+        execute(msg.sender);
+        vm.stopBroadcast();
+    }
+
+    function execute(address broadcastFrom) public {
+        EOJsonUtils.Config memory configStructured = EOJsonUtils.getParsedConfig();
+
+        require(configStructured.targetChainId == block.chainid, "Wrong chain id for this config.");
+
+        require(
+            configStructured.eoracleChainId == vm.envUint("EORACLE_CHAIN_ID"), "Wrong EORACLE_CHAIN_ID for this config."
+        );
+
+        string memory outputConfig = EOJsonUtils.initOutputConfig();
+        address timelock = outputConfig.readAddress(".timelock");
+        address bls = outputConfig.readAddress(".bls");
+
+        address feedVerifierProxy =
+            deployFeedVerifier(timelock, broadcastFrom, IBLS(bls), configStructured.eoracleChainId);
+        EOJsonUtils.OUTPUT_CONFIG.serialize("feedVerifier", feedVerifierProxy);
+
+        address implementationAddress = Upgrades.getImplementationAddress(feedVerifierProxy);
+        string memory outputConfigJson =
+            EOJsonUtils.OUTPUT_CONFIG.serialize("feedVerifierImplementation", implementationAddress);
+
+        EOJsonUtils.writeConfig(outputConfigJson);
+    }
+}
---
+++ script/timelocked/UpgradeFeedManagerAndAdaptersTimelocked.s.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import { ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import { EOFeedFactoryBeacon } from "../../src/adapters/factories/EOFeedFactoryBeacon.sol";
+import { Script } from "forge-std/Script.sol";
+import { stdJson } from "forge-std/Script.sol";
+import { EOJsonUtils } from "../utils/EOJsonUtils.sol";
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+import { TimelockBase } from "./TimelockBase.sol";
+import { EOFeedManager } from "../../src/EOFeedManager.sol";
+import { EOFeedAdapterV2 } from "../../src/adapters/EOFeedAdapterV2.sol";
+
+contract UpgradeFeedManagerAndAdaptersTimelocked is Script, TimelockBase {
+    using stdJson for string;
+
+    struct LocalVars {
+        address feedManager;
+        address admin;
+        address feedRegistryAdapter;
+        address bls;
+        address implementationAddress;
+        TimelockController timelock;
+        string outputConfig;
+        bytes initData;
+        bytes payload;
+        address[] targets;
+        bytes[] payloads;
+        uint256[] values;
+        bytes32 salt;
+        bytes32 predecessor;
+        uint256 delay;
+    }
+
+    LocalVars public vars;
+
+    function run(bool isExecution) external {
+        vm.startBroadcast();
+        execute(isExecution, true);
+        vm.stopBroadcast();
+    }
+
+    function execute(bool isExecution, bool send) public returns (bytes memory) {
+        EOJsonUtils.Config memory configStructured = EOJsonUtils.getParsedConfig();
+        vars.outputConfig = EOJsonUtils.initOutputConfig();
+        vars.feedManager = vars.outputConfig.readAddress(".feedManager");
+        vars.feedRegistryAdapter = vars.outputConfig.readAddress(".feedRegistryAdapter");
+        vars.bls = vars.outputConfig.readAddress(".bls");
+        vars.timelock = TimelockController(payable(vars.outputConfig.readAddress(".timelock")));
+        vars.admin = Upgrades.getAdminAddress(vars.feedManager);
+
+        // 1. Upgrade feed manager
+        if (isExecution) {
+            vars.implementationAddress = vars.outputConfig.readAddress(".feedManagerImplementation");
+        } else {
+            vars.implementationAddress = address(new EOFeedManager());
+            string memory outputConfigJson =
+                EOJsonUtils.OUTPUT_CONFIG.serialize("feedManagerImplementation", vars.implementationAddress);
+            EOJsonUtils.writeConfig(outputConfigJson);
+        }
+
+        vars.payload = abi.encodeCall(
+            ProxyAdmin(vars.admin).upgradeAndCall,
+            (ITransparentUpgradeableProxy(payable(vars.feedManager)), vars.implementationAddress, vars.initData)
+        );
+
+        vars.payloads.push(vars.payload);
+        vars.targets.push(vars.admin);
+        vars.values.push(0);
+
+        // 2. Set pauser registry
+        address pauserRegistry = vars.outputConfig.readAddress(".pauserRegistry");
+        vars.payload = abi.encodeCall(EOFeedManager(vars.feedManager).setPauserRegistry, (pauserRegistry));
+        vars.payloads.push(vars.payload);
+        vars.targets.push(vars.feedManager);
+        vars.values.push(0);
+
+        // 3. Set feed deployer
+        address feedDeployer = configStructured.feedDeployer;
+        vars.payload = abi.encodeCall(EOFeedManager(vars.feedManager).setFeedDeployer, (feedDeployer));
+        vars.payloads.push(vars.payload);
+        vars.targets.push(vars.feedManager);
+        vars.values.push(0);
+
+        // 4. Set feed verifier
+        address feedVerifier = vars.outputConfig.readAddress(".feedVerifier");
+        vars.payload = abi.encodeCall(EOFeedManager(vars.feedManager).setFeedVerifier, (feedVerifier));
+        vars.payloads.push(vars.payload);
+        vars.targets.push(vars.feedManager);
+        vars.values.push(0);
+
+        // 5. Upgrade beacon proxy
+        if (isExecution) {
+            vars.implementationAddress = vars.outputConfig.readAddress(".feedAdapterImplementation");
+        } else {
+            vars.implementationAddress = address(new EOFeedAdapterV2());
+            string memory outputConfigJson =
+                EOJsonUtils.OUTPUT_CONFIG.serialize("feedAdapterImplementation", vars.implementationAddress);
+            EOJsonUtils.writeConfig(outputConfigJson);
+        }
+        address beacon = EOFeedFactoryBeacon(vars.feedRegistryAdapter).getBeacon();
+        vars.payload = abi.encodeCall(UpgradeableBeacon(beacon).upgradeTo, (vars.implementationAddress));
+        vars.payloads.push(vars.payload);
+        vars.targets.push(beacon);
+        vars.values.push(0);
+
+        // schedule or execute
+        bytes memory txn = callTimelockBatch(
+            vars.timelock, isExecution, send, vars.targets, vars.payloads, vars.values, "feedManagerUpgrade"
+        );
+
+        delete vars;
+        return txn;
+    }
+}
