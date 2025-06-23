084d7e0044e6e9ab0901cb54d4f22744fe182f85
Tetiana Afonina <tetiana@eoracle.io>
2025-06-23 11:21:28 +0100

    feat: depracate eofeedadapter

--- src/interfaces/Errors.sol
+++ src/interfaces/Errors.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.25;
 /*//////////////////////////////////////////////////////////////////////////
                                 EOFeedManager
 //////////////////////////////////////////////////////////////////////////*/
+error Deprecated();
 error CallerIsNotWhitelisted(address caller);
 error MissingLeafInputs();
 error FeedNotSupported(uint16 feedId);
